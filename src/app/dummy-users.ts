1d0bf7c62a493361b048140718cf4361168c14a7
Sahithyavangala <sahithyavangala86@gmail.com>
2024-11-12 18:08:47 +0530

    Initial commit

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+    {
+      id: 'u1',
+      name: 'Vangala Sahithya',
+      avatar: 'user-1.jpg',
+    },
+    {
+      id: 'u2',
+      name: 'Saketh Chaitanya',
+      avatar: 'user-2.jpg',
+    },
+    {
+      id: 'u3',
+      name: 'Sathvik Reddy',
+      avatar: 'user-3.jpg',
+    },
+    {
+      id: 'u4',
+      name: 'Polagani Manoj',
+      avatar: 'user-4.jpg',
+    },
+    {
+      id: 'u5',
+      name: 'Santoshi Yellenki',
+      avatar: 'user-5.jpg',
+    },
+    {
+      id: 'u6',
+      name: 'Hitesh Chandra',
+      avatar: 'user-6.jpg',
+    },
+  ];
